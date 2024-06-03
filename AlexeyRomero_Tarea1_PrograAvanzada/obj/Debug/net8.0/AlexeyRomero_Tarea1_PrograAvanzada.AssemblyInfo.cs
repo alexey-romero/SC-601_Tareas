edfdd71a1f374d9dd88baf8bbca1a1ad34da0e85
Alexey Romero <alexey.rs@outlook.com>
2024-06-02 20:46:24 -0600

    gitignore files

--- AlexeyRomero_Tarea1_PrograAvanzada/obj/Debug/net8.0/AlexeyRomero_Tarea1_PrograAvanzada.AssemblyInfo.cs
+++ AlexeyRomero_Tarea1_PrograAvanzada/obj/Debug/net8.0/AlexeyRomero_Tarea1_PrograAvanzada.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlexeyRomero_Tarea1_PrograAvanzada")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7595c10c4c7f643ea1dea0601f32df0b7973803c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlexeyRomero_Tarea1_PrograAvanzada")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlexeyRomero_Tarea1_PrograAvanzada")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
